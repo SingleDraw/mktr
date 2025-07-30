ed1bfd5ef73a986e097df81fe26f580a5be1831b
Kamil Malek <truckdriverbuck@gmail.com>
2025-07-30 06:01:57 +0200

    fixes missing main function

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="mktr",
-    version="0.1.2",
+    version="0.1.3",
     python_requires=">=3.6",
     license="MIT",
     description="Convert tree structure into actual folders/files via GUI or CLI",
