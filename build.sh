85424ff31fde8069c309df68d44e2f27e157fd3a
Kamil Malek <truckdriverbuck@gmail.com>
2025-07-30 06:10:59 +0200

    adds little reminder

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# This script builds and uploads the mktr package to PyPI.
+# Dont forget to update the version in mktr/__init__.py and setup.py before running this script.
 rm -rf build dist mktr.egg-info
 python -m build
 twine upload dist/*
